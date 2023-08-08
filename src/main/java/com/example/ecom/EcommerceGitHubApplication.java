a17b75b6c8c6cc9218b49fe13c4413120ecbcf5f
radhi <radhi@host.docker.internal>
2023-08-08 13:22:40 +0300

    First commit

---
+++ src/main/java/com/example/ecom/EcommerceGitHubApplication.java
@@ -0,0 +1,14 @@
+package com.example.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGitHubApplication.class, args);
+		System.out.println("Hii");
+	}
+
+}
